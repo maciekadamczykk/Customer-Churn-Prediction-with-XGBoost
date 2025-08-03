d33224a40991223a67aa5eaa32886cfd03f31bb4
maciekadamczykk <maciek.adamczyk2004@gmail.com>
2025-08-03 15:41:25 +0200

    Data cleaning, cleaned csv data

---
+++ data_cleaning.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.preprocessing import OrdinalEncoder
+
+df = pd.read_csv("data/rawdata.csv")
+
+newdf = df.drop("customerID", axis="columns")
+
+newdf['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+
+categorical_cols = newdf.select_dtypes(include=['object', 'category']).columns.to_list()
+
+encoder = OrdinalEncoder()
+encooded_categories = encoder.fit_transform(newdf[categorical_cols])
+
+newdf[categorical_cols] = encooded_categories
+
+print(newdf.head())
+
+newdf.to_csv("data/processed_data.csv", index=False)
+
